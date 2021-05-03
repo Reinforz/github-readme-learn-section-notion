85dadbecd1e44abad1dc0450df89fc4eca26eb22
Safwan Shaheer <devorein00@gmail.com>
2021-05-03 23:20:58 +0600

    Added tests for constructCategoriesMap module

---
+++ tests/utils/constructCategoriesMap.test.ts
@@ -0,0 +1,45 @@
+import { constructCategoriesMap } from '../../src/utils/constructCategoriesMap';
+
+it(`Should work`, () => {
+  const option_1 = {
+      color: 'blue',
+      value: 'A'
+    } as any,
+    option_2 = {
+      color: 'yellow',
+      value: 'C'
+    } as any,
+    option_3 = {
+      color: 'red',
+      value: 'B'
+    } as any;
+  const categories_map = constructCategoriesMap({
+    name: 'Options',
+    options: [option_1, option_2, option_3],
+    type: 'multi_select'
+  });
+
+  expect(Array.from(categories_map.entries())).toStrictEqual([
+    [
+      'A',
+      {
+        items: [],
+        ...option_1
+      }
+    ],
+    [
+      'B',
+      {
+        items: [],
+        ...option_3
+      }
+    ],
+    [
+      'C',
+      {
+        items: [],
+        ...option_2
+      }
+    ]
+  ]);
+});
