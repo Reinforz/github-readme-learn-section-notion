54a8873f771fd58a4a50adfcd70c50d267ce6e11
Safwan Shaheer <devorein00@gmail.com>
2022-01-22 12:21:09 +0600

    Added error message if step fails

--- dist/index.js
+++ dist/index.js
@@ -1668,59 +1668,67 @@ var action_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _a
 
 function action() {
     return action_awaiter(this, void 0, void 0, function* () {
-        const NOTION_TOKEN_V2 = core.getInput('token_v2');
-        let id = core.getInput('database_id').replace(/-/g, '');
-        const databaseId = `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(16, 4)}-${id.substr(20)}`;
-        const headers = {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            cookie: `token_v2=${NOTION_TOKEN_V2}`
-        };
-        const http = new http_client/* HttpClient */.eN(undefined, undefined, {
-            headers
-        });
-        const collectionView = yield ActionUtils.fetchData(databaseId, 'block', http);
-        core.info('Fetched database');
-        const collection_id = collectionView.collection_id;
-        const collection = yield ActionUtils.fetchData(collection_id, 'collection', http);
-        core.info('Fetched collection');
-        const response = yield http.post(`https://www.notion.so/api/v3/queryCollection`, JSON.stringify({
-            collection: {
-                id: collection_id,
-                spaceId: collectionView.space_id
-            },
-            collectionView: {
-                id: collectionView.view_ids[0],
-                spaceId: collectionView.space_id
-            },
-            query: {},
-            loader: {
-                type: 'table',
-                loadContentCover: false,
-                limit: 10000,
-                userTimeZone: ''
-            }
-        }));
-        const { recordMap } = JSON.parse(yield response.readBody());
-        core.info('Fetched rows');
-        const { schema } = collection;
-        const [category_schema_entry, color_schema_entry] = ActionUtils.getSchemaEntries(schema);
-        const rows = ActionUtils.modifyRows(recordMap, databaseId);
-        const categories_map = ActionUtils.constructCategoriesMap(category_schema_entry[1]);
-        ActionUtils.populateCategoriesMapItems(rows, category_schema_entry[0], categories_map);
-        const README_PATH = `${process.env.GITHUB_WORKSPACE}/README.md`;
-        core.info(`Reading from ${README_PATH}`);
-        const readmeLines = external_fs_default().readFileSync(README_PATH, 'utf-8').split('\n');
-        const [startIdx, endIdx] = ActionUtils.checkForSections(readmeLines);
-        const newLines = ActionUtils.constructNewContents(categories_map, color_schema_entry[0]);
-        const finalLines = [
-            ...readmeLines.slice(0, startIdx + 1),
-            ...newLines,
-            ...readmeLines.slice(endIdx)
-        ];
-        core.info(`Writing to ${README_PATH}`);
-        external_fs_default().writeFileSync(README_PATH, finalLines.join('\n'), 'utf-8');
-        yield ActionUtils.commitFile();
+        try {
+            const NOTION_TOKEN_V2 = core.getInput('token_v2');
+            let id = core.getInput('database_id').replace(/-/g, '');
+            const databaseId = `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(16, 4)}-${id.substr(20)}`;
+            const headers = {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                cookie: `token_v2=${NOTION_TOKEN_V2}`
+            };
+            const http = new http_client/* HttpClient */.eN(undefined, undefined, {
+                headers
+            });
+            const collectionView = yield ActionUtils.fetchData(databaseId, 'block', http);
+            core.info('Fetched database');
+            const collection_id = collectionView.collection_id;
+            const collection = yield ActionUtils.fetchData(collection_id, 'collection', http);
+            core.info('Fetched collection');
+            const response = yield http.post(`https://www.notion.so/api/v3/queryCollection`, JSON.stringify({
+                collection: {
+                    id: collection_id,
+                    spaceId: collectionView.space_id
+                },
+                collectionView: {
+                    id: collectionView.view_ids[0],
+                    spaceId: collectionView.space_id
+                },
+                loader: {
+                    type: 'reducer',
+                    reducers: {
+                        collection_group_results: {
+                            type: 'results'
+                        }
+                    },
+                    searchQuery: '',
+                    userTimeZone: 'Asia/Dhaka'
+                }
+            }));
+            const { recordMap } = JSON.parse(yield response.readBody());
+            core.info('Fetched rows');
+            const { schema } = collection;
+            const [category_schema_entry, color_schema_entry] = ActionUtils.getSchemaEntries(schema);
+            const rows = ActionUtils.modifyRows(recordMap, databaseId);
+            const categories_map = ActionUtils.constructCategoriesMap(category_schema_entry[1]);
+            ActionUtils.populateCategoriesMapItems(rows, category_schema_entry[0], categories_map);
+            const README_PATH = `${process.env.GITHUB_WORKSPACE}/README.md`;
+            core.info(`Reading from ${README_PATH}`);
+            const readmeLines = external_fs_default().readFileSync(README_PATH, 'utf-8').split('\n');
+            const [startIdx, endIdx] = ActionUtils.checkForSections(readmeLines);
+            const newLines = ActionUtils.constructNewContents(categories_map, color_schema_entry[0]);
+            const finalLines = [
+                ...readmeLines.slice(0, startIdx + 1),
+                ...newLines,
+                ...readmeLines.slice(endIdx)
+            ];
+            core.info(`Writing to ${README_PATH}`);
+            external_fs_default().writeFileSync(README_PATH, finalLines.join('\n'), 'utf-8');
+            yield ActionUtils.commitFile();
+        }
+        catch (err) {
+            core.setFailed(err.message);
+        }
     });
 }
 
